45603a1eeae4bad74779fffc2ebd58b70d861fef
retrio-radio-androidstudio <a.wolfgramm@hotmail.com>
2026-05-29 21:20:52 +0200

    Add website to S3 bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "alina-aws-cloud-project-bucket"
 }
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "${path.module}/website/index.html"
+  content_type = "text/html"
+}
